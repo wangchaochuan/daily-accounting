f5def2b5c47dde5acba7c14c75c9263d424240fd
wangcci <wangcci@enn.cn>
2024-07-18 00:10:44 +0800

    fix: 修复备注过长时展示异常的问题

--- components/date-card.vue
+++ components/date-card.vue
@@ -1,141 +1,151 @@
 <template>
-	<view class="card">
-		<view class="title">
-			<view class="left">{{date}}</view>
-			<view class="right">{{expend}}</view>
-		</view>
-		<view class="content">
-			<view class="item" v-for="item in data" :key="item._id" @click="jump(item._id)">
-				<view class="left">
-					<view class="classify">
-						<text class="text">{{item.classify}}</text>
-						<u-tag :text="item.tag" v-if="item.tag" plain size="mini" shape="circle"></u-tag>
-					</view>
-					<view class="remark" v-if="item.remark">{{item.remark}}</view>
-				</view>
-				<view class="right" :class="{'income':item.type==='income'}">
-					￥{{item.amount}}
-				</view>
-			</view>
-		</view>
-	</view>
+  <view class="card">
+    <view class="title">
+      <view class="left">{{ date }}</view>
+      <view class="right">{{ expend }}</view>
+    </view>
+    <view class="content">
+      <view
+        class="item"
+        v-for="item in data"
+        :key="item._id"
+        @click="jump(item._id)"
+      >
+        <view class="left">
+          <view class="classify">
+            <text class="text">{{ item.classify }}</text>
+            <u-tag
+              :text="item.tag"
+              v-if="item.tag"
+              plain
+              size="mini"
+              shape="circle"
+            ></u-tag>
+          </view>
+          <view class="remark" v-if="item.remark">{{ item.remark }}</view>
+        </view>
+        <view class="right" :class="{ income: item.type === 'income' }">
+          ￥{{ item.amount }}
+        </view>
+      </view>
+    </view>
+  </view>
 </template>
 
 <script setup>
-	import {
-		computed
-	} from 'vue'
-	import dayjs from 'dayjs'
-	const props = defineProps({
-		date: {
-			type: String,
-			default: ''
-		},
-		data: {
-			type: Array,
-			default: () => []
-		}
-	})
-	const date = computed(() => {
-		const d = dayjs(props.date).day()
-		const week = ['日', '一', '二', '三', '四', '五', '六']
-		return `${props.date} (星期${week[d]})`
-	})
-	const expend = computed(() => {
-		const expendList = props.data.filter(v => v.type === 'expend');
-		let sum = 0;
-		expendList.forEach(v => {
-			sum += v.amount
-		})
-		return sum === 0 ? '' : `支出:￥${sum.toFixed(2)}`
-	})
-	const jump = (id) => {
-		uni.navigateTo({
-			url: `/pages/record-detail/record-detail?id=${id}`
-		})
-	}
+import { computed } from "vue";
+import dayjs from "dayjs";
+const props = defineProps({
+  date: {
+    type: String,
+    default: "",
+  },
+  data: {
+    type: Array,
+    default: () => [],
+  },
+});
+const date = computed(() => {
+  const d = dayjs(props.date).day();
+  const week = ["日", "一", "二", "三", "四", "五", "六"];
+  return `${props.date} (星期${week[d]})`;
+});
+const expend = computed(() => {
+  const expendList = props.data.filter((v) => v.type === "expend");
+  let sum = 0;
+  expendList.forEach((v) => {
+    sum += v.amount;
+  });
+  return sum === 0 ? "" : `支出:￥${sum.toFixed(2)}`;
+});
+const jump = (id) => {
+  uni.navigateTo({
+    url: `/pages/record-detail/record-detail?id=${id}`,
+  });
+};
 </script>
 
 <style lang="scss">
-	.card {
-		width: 100%;
-		margin-bottom: 16rpx;
+.card {
+  width: 100%;
+  margin-bottom: 16rpx;
 
-		.title {
-			height: 48rpx;
-			display: flex;
-			justify-content: space-between;
-			align-items: center;
+  .title {
+    height: 48rpx;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 
-			.left {
-				font-size: 28rpx;
-				color: $u-content-color;
-			}
+    .left {
+      font-size: 28rpx;
+      color: $u-content-color;
+    }
 
-			.right {
-				font-size: 24rpx;
-				color: $u-light-color;
-			}
-		}
+    .right {
+      font-size: 24rpx;
+      color: $u-light-color;
+    }
+  }
 
-		.content {
-			height: fit-content;
-			display: flex;
-			flex-direction: column;
-			margin-top: 8rpx;
-			background-color: #fff;
-			border-radius: 16rpx;
-			box-shadow: 0px 0px 3px 1px rgba(0, 0, 0, 0.08);
+  .content {
+    height: fit-content;
+    display: flex;
+    flex-direction: column;
+    margin-top: 8rpx;
+    background-color: #fff;
+    border-radius: 16rpx;
+    box-shadow: 0px 0px 3px 1px rgba(0, 0, 0, 0.08);
 
-			.item {
-				height: fit-content;
-				display: flex;
-				padding: 16rpx 32rpx;
-				border-bottom: 1px solid $u-border-color;
+    .item {
+      height: fit-content;
+      display: flex;
+      padding: 16rpx 32rpx;
+      border-bottom: 1px solid $u-border-color;
 
-				.left {
-					flex: 2;
+      .left {
+        flex: 2;
 
-					.classify {
-						height: 48rpx;
-						display: flex;
-						align-items: center;
+        .classify {
+          height: 48rpx;
+          display: flex;
+          align-items: center;
 
-						.text {
-							margin-right: 16rpx;
-						}
-					}
+          .text {
+            margin-right: 16rpx;
+          }
+        }
 
-					.remark {
-						height: 48rpx;
-						display: flex;
-						align-items: center;
-						color: $u-light-color;
-					}
-				}
+        .remark {
+          height: fit-content;
+          line-height: 48rpx;
+          display: flex;
+          align-items: center;
+          color: $u-light-color;
+        }
+      }
 
-				.right {
-					height: 100%;
-					flex: 1;
-					display: flex;
-					justify-content: flex-end;
-					align-items: center;
-					align-self: center;
-					color: $u-error-disabled;
+      .right {
+        height: 100%;
+        flex: 1;
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        align-self: center;
+        color: $u-error-disabled;
 
-					&.income {
-						color: $u-success-disabled
-					}
-				}
+        &.income {
+          color: $u-success-disabled;
+        }
+      }
 
-				&:last-child {
-					border-bottom: none;
-				}
-			}
-		}
+      &:last-child {
+        border-bottom: none;
+      }
+    }
+  }
 
-		:deep(.u-tag) {
-			padding: 0 16rpx;
-		}
-	}
+  :deep(.u-tag) {
+    padding: 0 16rpx;
+  }
+}
 </style>
